b91b6e0a53f602c94f4f1aad3abd4fbf55317135
DilanMasMas <dilan.cardenas50324@gmail.com>
2019-06-10 01:27:20 -0400

    mas detalles

--- js/validar.js
+++ js/validar.js
@@ -1,25 +1,24 @@
-window.onload = "inicializar";
-var fautenticar;
-
-
-function inicializar(){
-    fautenticar = document.getElementById("form-aut");
-    fautenticar.addEventListener("submit", autentificar, false);
-
-}
-function autentificar(event){
-    alert("asdljflaksj");
-    var usuario = event.target.email.value;
-    var contrasena = event.target.password.value;
-
-    
-firebase.auth().sendSignInLinkToEmail(usuario, contrasena)
-.then(function() {
-
-  window.localStorage.setItem('emailForSignIn', usuario);
-  console.log("aaaa");
-})
-.catch(function(error) {
-    console.log("error en la auf");
+firebase.initializeApp({
+    apiKey: 'AIzaSyCJ1FQVvUkpOmoc1XLd4c3ahlu8IIIG4oU',
+    authDomain: 'eva2-f5936.firebaseapp.com',
+    projectId: 'eva2-f5936'
 });
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+
+function auth() {
+    var usuario = document.getElementById("email").value;
+    var contrasena = document.getElementById("password").value;
+
+    firebase.auth().sendSignInLinkToEmail(usuario, contrasena)
+        .then(function () {
+
+            window.localStorage.setItem('emailForSignIn', usuario);
+            console.log("aaaa");
+        })
+        .catch(function (error) {
+            console.log("error en la auf");
+        });
+
 }
